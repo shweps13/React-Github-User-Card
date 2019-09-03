b80d23482dcd6a74a519b68b91e7650335270436
Heorhii Siburov <g.siburov@gmail.com>
2019-09-03 13:14:24 -0700

    switched to component model of App

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -2,8 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+
+  constructor() {
+    super();
+  }
+
+
+  render() {
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -21,6 +29,8 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
 export default App;
+
