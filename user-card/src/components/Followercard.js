df0808d5a5f504bbc734b7c8a9c12f23ad0a4336
Heorhii Siburov <g.siburov@gmail.com>
2019-09-03 21:11:56 -0700

    added pics

--- user-card/src/components/Followercard.js
+++ user-card/src/components/Followercard.js
@@ -10,9 +10,12 @@ console.log("List of followers in child element", props)
             {props.followers.map(follower => (
                 
             <div key={follower.id}>
-                <h2>{follower.login}</h2>
-                <p>{follower.login}</p>
-                <a href={follower.html_url}>{follower.html_url}</a>
+                <img src={follower.avatar_url} alt={follower.login} />
+                <div>
+                    <h2>{follower.login}</h2>
+                    <p>{follower.login}</p>
+                    <a href={follower.html_url}>{follower.html_url}</a>
+                </div>
             </div>
           ))}
         </div>
