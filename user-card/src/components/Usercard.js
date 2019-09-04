6b3ad8c251f8bc6a69cd51a007b4e4565e24ae69
Heorhii Siburov <g.siburov@gmail.com>
2019-09-03 23:47:27 -0700

    some styling

--- user-card/src/components/Usercard.js
+++ user-card/src/components/Usercard.js
@@ -7,17 +7,19 @@ const tableWay = `http://ghchart.rshah.org/${props.cards.login}`
 
   return (
     <div className="Usercard">
-        <img src={props.cards.avatar_url} alt={props.cards.name} />
-        <div>
-            <h2>{props.cards.name}</h2>
-            <p>{props.cards.login}</p>
-            <a href={props.cards.html_url}>{props.cards.html_url}</a>
-            <p>Location: {props.cards.location}</p>
-            <p>Followers: {props.cards.followers}</p>
-            <p>Following: {props.cards.following}</p>
-            <p>Bio: {props.cards.bio}</p>
+        <div className="Upblock">
+            <img src={props.cards.avatar_url} alt={props.cards.name} />
+            <div className="Uptext">
+                <h2>{props.cards.name}</h2>
+                <p>{props.cards.login}</p>
+                <a href={props.cards.html_url}>{props.cards.html_url}</a>
+                <p>Location: {props.cards.location}</p>
+                <p>Followers: {props.cards.followers}</p>
+                <p>Following: {props.cards.following}</p>
+                <p>Bio: {props.cards.bio}</p>
+            </div>
         </div>
-        <img src={tableWay} />
+        <img className="Gittable" src={tableWay} />
     </div>
   );
 }
